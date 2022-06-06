7223c185d926e5f248645bf4ab9edaa9ba9b2a0a
Tukhvatulin Almaz <99902545+meowimhere@users.noreply.github.com>
2022-06-06 16:14:53 +0300

    7
    - disabled fixed header, because of the glitchy scroll upon clicking the menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 	$('.header__burger').click(function(event) {
+		event.preventDefault();
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 		$('html').toggleClass('lock');
 		$('.content').toggleClass('blur');
 		$('.content').click(function(event) {
+			event.preventDefault();
 			$('.header__burger,.header__menu').removeClass('active');
 			$('body').removeClass('lock');
 			$('.content').removeClass('blur');
