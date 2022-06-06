74f67ca5192d7a4c8c0a4f4866b36d18a60bf225
Tukhvatulin Almaz <99902545+meowimhere@users.noreply.github.com>
2022-06-06 15:25:48 +0300

    2
    - JS for comfortable usage for phones.
    - burger-menu animation
    - header is fixed

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 		$('.content').toggleClass('blur');
+		$('.content').click(function(event) {
+			$('.header__burger,.header__menu').removeClass('active');
+			$('body').removeClass('lock');
+			$('.content').removeClass('blur');
+		});
+		
+
+		
 	});
 });
