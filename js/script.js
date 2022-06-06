e84c271dcd49d8355ae9390a58eb309a70f702b3
Tukhvatulin Almaz <99902545+meowimhere@users.noreply.github.com>
2022-06-06 15:38:17 +0300

    test for phones

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
+		$('html').toggleClass('lock');
 		$('.content').toggleClass('blur');
 		$('.content').click(function(event) {
 			$('.header__burger,.header__menu').removeClass('active');
